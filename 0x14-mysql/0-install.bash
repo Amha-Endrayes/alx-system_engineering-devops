16136c040aaf2b7d91efd8dd507b9fef0e8b07da
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-11-10 04:59:40 +0300

    added sql config scripts

--- 0x14-mysql/0-install.bash
+++ 0x14-mysql/0-install.bash
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 # Installs MySQL server version 5.7.x
-sudo apt-key add './mysql-5.7_signature.key'
-sudo /bin/bash -c 'echo "deb http://repo.mysql.com/apt/ubuntu bionic mysql-5.7" >> /etc/apt/sources.list.d/mysql.list'
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb
+sudo apt-get update
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 467B942D3A79BD29
+cat <<- EOF > /etc/apt/sources.list.d/mysql.list
+deb http://repo.mysql.com/apt/ubuntu/ bionic mysql-5.7
+EOF
 sudo apt-get update
 sudo apt install -f mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7*
 # password: ubuntu_user_pwd
